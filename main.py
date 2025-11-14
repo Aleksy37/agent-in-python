b4f8d2ee322f1ab696cf515d91e5a0491afad948
Aleksy37 <aleksyveillette@gmail.com>
2025-11-14 15:58:45 -0500

    added support for custom prompt in cli + error handling

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 
-
-def generate_content(client):
-    response = client.models.generate_content(
+def generate_content(client, prompt):
+    return client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents= "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents= prompt
     )
+
+
+def main():
+    prompt = " ".join(sys.argv[1:]).strip()
+    if not prompt:
+        print("Please enter a prompt")
+        sys.exit(1)
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = generate_content(client, prompt)
     print(response.text)
     print(
         f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
         f"Response tokens: {response.usage_metadata.candidates_token_count}"  
     )
-
-
-
-def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-    generate_content(client)
 
 
 if __name__ == "__main__":
