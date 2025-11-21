c569c5f48ef686d3ccedcbdf889ca56442f8b650
Aleksy37 <aleksyveillette@gmail.com>
2025-11-21 15:30:04 -0500

    added verbose flag

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 import sys
 
 
-def generate_content(client, prompt):
+def generate_content(client, messages):
     return client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents= prompt
+        model="gemini-2.0-flash-001",
+        contents= messages
     )
 
 
 def main():
-    prompt = " ".join(sys.argv[1:]).strip()
-    if not prompt:
-        print("Please enter a prompt")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="AI agent CLI tool")
+    parser.add_argument("prompt", type=str, help="The user's prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable Verbose output")
+    args = parser.parse_args()
+    prompt = args.prompt
+    verbose = args.verbose
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        print("Error: Gemini api key not set in environment")
+        sys.exit(1)
     client = genai.Client(api_key=api_key)
-    response = generate_content(client, prompt)
+
+    if verbose:
+        print(f"User prompt: {prompt}")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
+    response = generate_content(client, messages)
+
     print(response.text)
-    print(
-        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-        f"Response tokens: {response.usage_metadata.candidates_token_count}"  
-    )
+
+    if verbose:
+        print(
+            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+            f"Response tokens: {response.usage_metadata.candidates_token_count}"  
+        )
 
 
 if __name__ == "__main__":
