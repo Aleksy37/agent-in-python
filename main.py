19e360cef2b2b44bffe7050e1b40c54d99c9fd5b
Aleksy37 <aleksyveillette@gmail.com>
2025-12-08 20:25:52 -0500

    added support for ai agent to make function declarations

--- main.py
+++ main.py
@@ -1,51 +1,59 @@
 import os
+import argparse
+
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import argparse
-import sys
+
 from prompts import system_prompt
-
-
-def generate_content(client, messages):
-    return client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents= messages,
-        config= types.GenerateContentConfig(system_instruction=system_prompt)
-    )
-
+from call_function import available_functions
 
 def main():
     parser = argparse.ArgumentParser(description="AI agent CLI tool")
     parser.add_argument("prompt", type=str, help="The user's prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable Verbose output")
     args = parser.parse_args()
-    prompt = args.prompt
-    verbose = args.verbose
+
     
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     if not api_key:
-        print("Error: Gemini api key not set in environment")
-        sys.exit(1)
+        raise RuntimeError("GEMINI_API_KEY env var not set")
+
     client = genai.Client(api_key=api_key)
-
-    if verbose:
-        print(f"User prompt: {prompt}")
-
     messages = [
-        types.Content(role="user", parts=[types.Part(text   =prompt)])
+        types.Content(role="user", parts=[types.Part(text=args.prompt)])
     ]
+    if args.verbose:
+        print(f"User prompt: {args.prompt}\n")
 
-    response = generate_content(client, messages)
+    generate_content(client, messages, args.verbose)
 
-    print(response.text)
 
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+    )
+
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
     if verbose:
         print(
             f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
             f"Response tokens: {response.usage_metadata.candidates_token_count}"  
         )
+
+    if not response.function_calls:
+        print("Response:")
+        print(response.text)
+    else:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
